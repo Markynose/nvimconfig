292cae03735f5867942ade763afbd3a41c4ff2ea
Mark Buchok <kkijq12089@gmail.com>
2026-01-12 19:13:48 +0200

    Initial Neovim config

---
+++ lua/plugins/rustaceanvim.lua
@@ -0,0 +1,14 @@
+return {
+  -- RustaceanVim
+  {
+    "mrcjkb/rustaceanvim",
+    version = "^6",
+    lazy = false,
+  },
+
+  -- Debug Adapter Protocol client
+  {
+    "mfussenegger/nvim-dap",
+    lazy = false,
+  },
+}
